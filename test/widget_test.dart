ce54e34908cce8089365a17a2c0448da5f26a5e7
sohukia <sohukia@gmail.com>
2023-01-13 18:53:06 +0100

    project set back to zero

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:DIW/main.dart';
+import 'package:diw/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
