8720a2f8bfa33ff319405bb6211510031defe920
sohukia <sohukia@gmail.com>
2023-01-15 14:07:11 +0100

    renamed MyApp to App

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
